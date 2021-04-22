18168d342bb21b84716db06e890c5735b4e709e2
jihwang <jihwaang@naver.com>
2021-04-23 00:37:09 +0900

    js modified

--- js/study/study-main-list.js
+++ js/study/study-main-list.js
@@ -4,7 +4,7 @@ function init(){
     item.forEach( ele => {
         ele.addEventListener('click', (e) =>{
             if(url.indexOf('127.0.0.1') === -1)
-                location.href = 'studyin/study/study-main-detail.html';
+                location.href = '/studyin/study/study-main-detail.html';
             else
                 location.href = '../study/study-main-detail.html';
         });
