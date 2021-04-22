66a6f034bf721a8302b15e13c82892ffe3edb7f9
jihwang <jihwaang@naver.com>
2021-04-23 00:03:31 +0900

    js modified

--- js/mypage/mystudy-list.js
+++ js/mypage/mystudy-list.js
@@ -1,7 +1,12 @@
 const init = () => {
     const group = document.querySelectorAll('.item');
     const url = location.hostname;
-    group.forEach(ele => ele.addEventListener('click', () => location.href = '../study/study-detail.html'));
+    group.forEach(ele => ele.addEventListener('click', () => {
+        if(url.indexOf('127.0.0.1') === -1)
+            location.href = '/studyin/study/study-detail.html'
+        else
+            location.href = '../study/study-detail.html'
+    }));
 
     const deleteBtn = document.querySelectorAll('.delete-btn');
     const popup = document.querySelector('.popup');
@@ -19,6 +24,5 @@ const init = () => {
         // e.stopPropagation();
         popup.classList.remove('active');
     }));
-}
-
+};
 init();
